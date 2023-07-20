3707e3eabc4efec3faba362e730cd06818ba1e4c
Md. Rifat Hossen Saown <rifatsaown0@gmail.com>
2023-07-20 09:35:50 +0600

    Stripe Added i backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.DB_PATH;
@@ -170,6 +171,21 @@ async function run() {
             const result = await cartCollection.deleteOne(query);
             res.json(result);
         })
+        // ---------Cart Colection End-------------//
+
+        // Payment Intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: price * 100,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
